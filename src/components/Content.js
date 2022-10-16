d77582df3a2cf382245c3b4ab5041ef13bcd538b
Tarun Sharma <tarunsharma0588@gmail.com>
2022-10-16 18:04:46 +0530

    second commit

--- src/components/Content.js
+++ src/components/Content.js
@@ -10,7 +10,7 @@ function Content() {
         <Box>
             <Section1 />
             <Section2 />
-            {/* <Section3 /> */}
+            <Section3 />
             <Section4 />
         </Box>
     )
